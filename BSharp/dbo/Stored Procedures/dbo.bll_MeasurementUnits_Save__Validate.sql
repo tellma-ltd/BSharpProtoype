a89e9cf7ad016e4c5aafde1c35180d93466efa9a
Mohamad Akra <mohamad.akra@banan-it.com>
2018-12-15 14:27:28 +0300

    Name uniqueness in Measurement Units

--- BSharp/dbo/Stored Procedures/dbo.bll_MeasurementUnits_Save__Validate.sql
+++ BSharp/dbo/Stored Procedures/dbo.bll_MeasurementUnits_Save__Validate.sql
@@ -25,14 +25,25 @@ SET NOCOUNT ON;
 		HAVING COUNT(*) > 1
 	)
 
-	-- Name must be unique
+	-- Name must not exist in the 
 	INSERT INTO @ValidationErrors([Key], [ErrorName], [Argument1], [Argument2], [Argument3], [Argument4], [Argument5]) 
 	SELECT '[' + CAST(FE.[Index] AS NVARCHAR(255)) + '].Name' As [Key], N'TheName{{0}}IsUsed' As [ErrorName],
 		FE.[Name] AS Argument1, NULL AS Argument2, NULL AS Argument3, NULL AS Argument4, NULL AS Argument5
 	FROM @Entities FE 
 	JOIN [dbo].MeasurementUnits BE ON FE.[Name] = BE.[Name]
 	WHERE (FE.[EntityState] = N'Inserted') OR (FE.Id <> BE.Id);
-		-- Add further logic
+
+	-- Name must be unique in the uploaded list
+	INSERT INTO @ValidationErrors([Key], [ErrorName], [Argument1], [Argument2], [Argument3], [Argument4], [Argument5]) 
+	SELECT '[' + CAST([Index] AS NVARCHAR(255)) + '].Name' As [Key], N'TheName0IsUsedInTheList' As [ErrorName],
+		[Name] AS Argument1, NULL AS Argument2, NULL AS Argument3, NULL AS Argument4, NULL AS Argument5
+	FROM @Entities
+	WHERE [Name] IN (
+		SELECT [Name]
+		FROM @Entities
+		GROUP BY [Name]
+		HAVING COUNT(*) > 1
+	)
 
 	SELECT @ValidationErrorsJson = 
 	(
