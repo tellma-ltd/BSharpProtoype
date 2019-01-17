b275668527a84e57c7ae8d53fdca340fc7590648
Mohamad Akra <mohamad.akra@banan-it.com>
2019-01-17 15:39:16 +0400

    Fixed Agent validation logic

--- BSharp/dbo/Stored Procedures/dbo.bll_Agents_Validate__Save.sql
+++ BSharp/dbo/Stored Procedures/dbo.bll_Agents_Validate__Save.sql
@@ -10,7 +10,7 @@ SET NOCOUNT ON;
     INSERT INTO @ValidationErrors([Key], [ErrorName], [Argument1])
     SELECT '[' + CAST([Id] AS NVARCHAR(255)) + '].Id' As [Key], N'Error_TheId0WasNotFound' As [ErrorName], CAST([Id] As NVARCHAR(255)) As [Argument1]
     FROM @Entities
-    WHERE Id Is NOT NULL AND Id NOT IN (SELECT Id from [dbo].[Custodies]);
+    WHERE Id Is NOT NULL AND Id NOT IN (SELECT Id from [dbo].[Custodies] WHERE CustodyType = N'Agent');
 
 	-- Code must be unique
 	INSERT INTO @ValidationErrors([Key], [ErrorName], [Argument1], [Argument2], [Argument3], [Argument4], [Argument5]) 
@@ -18,7 +18,8 @@ SET NOCOUNT ON;
 		FE.Code AS Argument1, NULL AS Argument2, NULL AS Argument3, NULL AS Argument4, NULL AS Argument5
 	FROM @Entities FE 
 	JOIN [dbo].[Custodies] BE ON FE.Code = BE.Code
-	WHERE (FE.Id IS NULL) OR (FE.Id <> BE.Id);
+	WHERE BE.CustodyType = N'Agent'
+	AND ((FE.Id IS NULL) OR (FE.Id <> BE.Id));
 
 	 --User Id must be unique
 	INSERT INTO @ValidationErrors([Key], [ErrorName], [Argument1], [Argument2], [Argument3], [Argument4], [Argument5]) 
@@ -27,6 +28,7 @@ SET NOCOUNT ON;
 	FROM @Entities FE 
 	JOIN [dbo].Custodies BE ON FE.UserId = BE.UserId
 	JOIN dbo.Users U ON FE.UserId = U.Id
-	WHERE (FE.Id IS NULL) OR (FE.Id <> BE.Id);
+	WHERE BE.CustodyType = N'Agent'
+	AND ((FE.Id IS NULL) OR (FE.Id <> BE.Id));
 
 	SELECT @ValidationErrorsJson = (SELECT * FROM @ValidationErrors	FOR JSON PATH);
